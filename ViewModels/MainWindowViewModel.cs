e1d448ea5cee5ae8213bf55d520565eafe25af6a
oscarcsg <velagomezoscar@gmail.com>
2026-02-22 22:03:02 +0100

    CHANGES:
    Created the FileItem model (class) with FileName, FilePath, FileExtension and FileSize attributes.
    A VERY basic UI with just a TextBox, a List and a TextBlock to show when the app is searching for the asked files.
    Implemented a basic and hardcoded logic that creates 10k files to try the list.
    
    ------------------------------------------------------
    
    To Do:
    
    1. Change the data paradigm to a Data Oriented Design. Currently the app uses regular objects which live in the Heap with the Object Header and Method Table. Because of the Object Oriented Design, a few files takes so much RAM (aprox. 550MB for just 1 million files).
    
    ------------------------------------------------------
    
    Version: 0.1.0

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,78 @@
-﻿namespace Lynx.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lynx.Models;
+using System.Collections.ObjectModel;
+
+namespace Lynx.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        // ------------------------------ //
+        //           ATTRIBUTES           //
+        // ------------------------------ //
+        // User input to search
+        [ObservableProperty]
+        private string _searchQuery = string.Empty;
+        // Flag to show when it is searching
+        [ObservableProperty]
+        private bool _isSearching;
+        // The list that will be drawn by the UI
+        public ObservableCollection<FileItem> SearchResults { get; } = new();
+
+
+
+        // ------------------------------ //
+        //            COMMANDS            //
+        // ------------------------------ //
+        // The search command in background
+        [RelayCommand]
+        private async Task PerformSearchAsync(string query)
+        {
+            // Set the searching flag true
+            IsSearching = true;
+
+            // Clean previous results
+            SearchResults.Clear();
+
+            // --------- SIMULATION ---------
+            // Generate 10k fake files in a secondary thread
+            await Task.Run(async () =>
+            {
+                await Task.Delay(150); // Simulates disc reading time
+                var rand = new Random();
+                for (int i = 0; i < 1000000; i++)
+                {
+                    SearchResults.Add(new FileItem
+                    {
+                        FileName = $"Archivo_Ultra_Rapido_{query}_{i}.txt",
+                        FilePath = $@"C:\Datos\{query}\Subcarpeta\",
+                        FileExtension = ".txt",
+                        FileSize = rand.Next()
+                    });
+                }
+            });
+
+            // Set the searching flag false
+            IsSearching = false;
+        }
+
+
+
+        // ------------------------------ //
+        //            METHODS             //
+        // ------------------------------ //
+        // Will be automatically executed when the user writes anything in the UI's search field
+        partial void OnSearchQueryChanged(string value)
+        {
+            // If there is no text in the field, clean the list
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                SearchResults.Clear();
+                return;
+            }
+
+            // Start the search without blocking the UI
+            PerformSearchCommand.Execute(value);
+        }
     }
 }
